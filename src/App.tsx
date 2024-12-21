0196261c1117bdf9b17d0f47ef74c0dfa186da0f
garden905 <balboa.0905@gmail.com>
2024-12-22 00:57:19 +0900

    テキスト読み取り機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import "./App.css";
-
+import { ChangeEvent, useState } from "react";
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checed: boolean;
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Todoリスト</h2>
         <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" />
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            className="inputText"
+          />
           <input type="submit" value="作成" className="submitButton" />
         </form>
       </div>
